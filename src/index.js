5eff2c54e363a61d47f019ecdaf05103cd8c978a
DavidAvigad <david.avigad@gmail.com>
2022-11-02 17:44:48 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // This route processes GET requests to "/"`
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.send(
     '<h1>REST API</h1><p>A REST API starter using Express and body-parser.<br /><br />To test, curl the following and view the terminal logs:<br /><br /><i>curl -H "Content-Type: application/json" -X POST -d \'{"username":"test","data":"1234"}\' https://' +
       url[2] +
@@ -17,8 +17,9 @@ app.get("/", function(req, res) {
   console.log("Received GET");
 });
 
+app.get("/test", (req, res) => res.status(200).send("all good 🙏"));
 // A route for POST requests sent to `/update`
-app.post("/update", function(req, res) {
+app.post("/update", function (req, res) {
   if (!req.body.username || !req.body.data) {
     console.log("Received incomplete POST: " + JSON.stringify(req.body));
     return res.send({ status: "error", message: "missing parameter(s)" });
@@ -29,10 +30,10 @@ app.post("/update", function(req, res) {
 });
 
 // A GET request handler for `/update`
-app.get("/update", function(req, res) {
+app.get("/update", function (req, res) {
   var dummyData = {
     username: "testUser",
-    data: "1234"
+    data: "1234",
   };
   console.log("Received GET: " + JSON.stringify(req.body));
   if (!req.query.username) {
@@ -47,6 +48,6 @@ app.get("/update", function(req, res) {
 });
 
 // Listen on port 8080
-var listener = app.listen(8080, function() {
+var listener = app.listen(8080, function () {
   console.log("Listening on port " + listener.address().port);
 });
